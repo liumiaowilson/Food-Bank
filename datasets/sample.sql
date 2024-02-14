70a2322f877be3225e3cee3b1b120f979d7beec1
Wilson Liu <lockleeliumiaowilson@gmail.com>
2024-02-14 04:44:27 +0000

    Add demo dataset.

---
+++ datasets/sample.sql
@@ -0,0 +1,27 @@
+BEGIN TRANSACTION;
+CREATE TABLE "Account" (
+	id INTEGER NOT NULL, 
+	"Name" VARCHAR(255), 
+	"ParentId" VARCHAR(255), 
+	PRIMARY KEY (id)
+);
+INSERT INTO "Account" VALUES(1,'Sample Account for Entitlements','');
+INSERT INTO "Account" VALUES(2,'PizzaHut','');
+CREATE TABLE "Delivery_Item__c" (
+	id INTEGER NOT NULL, 
+	"Food_Expiration_Date__c" VARCHAR(255), 
+	"Food_Storage__c" VARCHAR(255), 
+	"Name" VARCHAR(255), 
+	"Delivery__c" VARCHAR(255), 
+	PRIMARY KEY (id)
+);
+INSERT INTO "Delivery_Item__c" VALUES(1,'2024-02-15','Non-refrigerated','Small Pizza','1');
+CREATE TABLE "Delivery__c" (
+	id INTEGER NOT NULL, 
+	"Scheduled_Date__c" VARCHAR(255), 
+	"Status__c" VARCHAR(255), 
+	"Supplier__c" VARCHAR(255), 
+	PRIMARY KEY (id)
+);
+INSERT INTO "Delivery__c" VALUES(1,'2024-02-14T01:00:00.000+0000','Scheduled','2');
+COMMIT;
